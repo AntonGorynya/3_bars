9fd114e58e09c2398675e98f15b806292556e8be
Anton <antongorynya1@gmail.com>
2017-10-09 17:37:23 +0300

    simplify code

--- bars.py
+++ bars.py
@@ -4,6 +4,11 @@ import json
 import argparse
 
 
+def get_key(input_dict,input_value):
+	for k,v in input_dict.items():
+		if v == input_value:
+			return k
+
 def load_data(filepath):
     with open(filepath, encoding='utf-8') as f:
         json_data = json.load(f)
@@ -11,72 +16,26 @@ def load_data(filepath):
 
 
 def get_biggest_bar(json_data):
-    biggest_seats = 0
-    biggest_name = ['']
+    bar = max(json_data['features'], key=lambda x:x['properties']['Attributes']['SeatsCount'])
+    seats_count = bar['properties']['Attributes']['SeatsCount']
     for bar in json_data['features']:
-        biggest_name_candidate = (bar['properties']['Attributes']['Name'])
-        biggest_seats_candidate = (bar['properties']['Attributes']
-                                   ['SeatsCount'])
-        if biggest_seats == biggest_seats_candidate:
-             biggest_name.append(biggest_name_candidate)
-        elif biggest_seats < max(biggest_seats, biggest_seats_candidate):
-            biggest_seats = max(biggest_seats, biggest_seats_candidate)
-            biggest_name = [biggest_name_candidate]
-    if len(biggest_name) == 1:
-        print('biggest bar is', biggest_name[0])
-        print('biggest seats counts is ', biggest_seats)
-    else:
-        print('biggests bar are ')
-        for bar in biggest_name:
-            print('- ', bar)
-        print('biggest seats counts is ', biggest_seats, end='\n\n')
+        if  bar['properties']['Attributes']['SeatsCount']==seats_count:
+            print(bar['properties']['Attributes']['Name'],bar['properties']['Attributes']['SeatsCount'])
 
 
 def get_smallest_bar(json_data):
-    smallest_seats = (json_data['features'][0]
-                      ['properties']['Attributes']['SeatsCount'])
-    smallest_name = [json_data['features'][0]['properties']['Attributes']['Name']]
+    bar = min(json_data['features'], key=lambda x: x['properties']['Attributes']['SeatsCount'])
+    seats_count = bar['properties']['Attributes']['SeatsCount']
     for bar in json_data['features']:
-        smallest_name_candidate = (bar['properties']['Attributes']['Name'])
-        smallest_seats_candidate = (bar['properties']
-                                    ['Attributes']['SeatsCount'])
-        if smallest_seats == smallest_seats_candidate:
-            smallest_name.append(smallest_name_candidate)
-        elif smallest_seats > min(smallest_seats, smallest_seats_candidate):
-            smallest_seats = min(smallest_seats, smallest_seats_candidate)
-            smallest_name = [smallest_name_candidate]
-    if len(smallest_name) == 1:
-        print('smallest bar is', smallest_name[0])
-        print('smallest seats counts is ', smallest_seats)
-    else:
-        print('smallest bar are ')
-        for bar in smallest_name:
-            print('- ', bar)
-        print('smallest seats counts is ', smallest_seats, end='\n\n')
+        if bar['properties']['Attributes']['SeatsCount'] == seats_count:
+            print(bar['properties']['Attributes']['Name'], bar['properties']['Attributes']['SeatsCount'])
 
 
 def get_closest_bar(json_data, longitude, latitude):
-    [bar_longitude, bar_latitude] = (json_data['features'][0]
-                                     ['geometry']['coordinates'])
-    closset_bar_name = [json_data['features'][0]['properties']['Attributes']['Name']]
-    for bar in json_data['features']:
-        closest_name_candidate = bar['properties']['Attributes']['Name']
-        coordinate_candidate = bar['geometry']['coordinates']
+    bar = min(json_data['features'], key=lambda x: (x['geometry']['coordinates'][0]-longitude)**2-(x['geometry']['coordinates'][1]-latitude)**2)
+    seats_count = bar['properties']['Attributes']['SeatsCount']
+    print(bar['properties']['Attributes']['Name'], seats_count)
 
-        if min(((coordinate_candidate[0]-longitude)**2+(coordinate_candidate[1]-latitude)**2),
-                ((bar_longitude-longitude)**2+(bar_latitude-latitude)**2)) < \
-                ((bar_longitude-longitude)**2+(bar_latitude-latitude)**2):
-            closset_bar_name = [closest_name_candidate]
-            [bar_longitude, bar_latitude] = coordinate_candidate
-        elif ((coordinate_candidate[0] - longitude) ** 2 + (coordinate_candidate[1] - latitude) ** 2) == \
-                ((bar_longitude - longitude) ** 2 + (bar_latitude - latitude) ** 2):
-            closset_bar_name.append(closest_name_candidate)
-    if len(closset_bar_name) == 1:
-        print('Closset bar is', closset_bar_name[0])
-    else:
-        print('Clossets bar are ')
-        for bar in closset_bar_name:
-            print('- ', bar)
 
 
 def create_parser():
